a41153dd39512d5a9cdc7d06f1122187c8aecac1
manoj_dhanorkar <manoj_dhanorkar@persistent.com>
2020-05-19 14:04:07 +0530

    monitoring : add type to scheme

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -17,6 +17,7 @@ import (
 	"github.com/persistentsys/mariadb-operator/pkg/controller"
 	"github.com/persistentsys/mariadb-operator/version"
 
+	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	kubemetrics "github.com/operator-framework/operator-sdk/pkg/kube-metrics"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
@@ -124,6 +125,12 @@ func main() {
 
 	// Setup all Controllers
 	if err := controller.AddToManager(mgr); err != nil {
+		log.Error(err, "")
+		os.Exit(1)
+	}
+
+	// Setup Scheme for all monitoring resources
+	if err := monitoringv1.AddToScheme(mgr.GetScheme()); err != nil {
 		log.Error(err, "")
 		os.Exit(1)
 	}
